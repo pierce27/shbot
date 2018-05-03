f679348a884289b931c4d4be7840b0c76a5defd1
Andrew Pierce <andrewpierce@Andrews-MacBook-Air.local>
2018-05-03 14:45:58 -0400

    Testing challenge

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 app = require('express')();
 
 app.get('/', function (req, res) {
-  res.send('hello world')
+  // res.send('hello world')
+  console.log(req)
+  res.send('hello wolrd')
 })
 
 app.listen(process.env.PORT || 3000)
