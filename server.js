77411a7ce70f457d798a0579a9356200b220c8a2
Andrew Pierce <andrewpierce@Andrews-MacBook-Air.local>
2018-05-03 16:41:38 -0400

    Test webhook

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 bodyParser = require('body-parser');
 app = require('express')();
 app.use(bodyParser.json());
+shgame = require('sh-game')
 
 app.post('/', function (req, res) {
   // res.send('hello world')
-  console.log(req.body)
-  res.send(req.body.challenge)
+	shgame.invite
+	res.send(200)  
+  // res.send(req.body.challenge)
 })
 
 app.get('/', function(req,res){
 	res.send('hellow world')
 })
 
+
 app.listen(process.env.PORT || 3000)
+
+
+https://hooks.slack.com/services/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
