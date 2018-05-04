1f31514c763acb7b761cb937a91ac1f5b31dfdd6
Andrew Pierce <andrewpierce@Andrews-MacBook-Air.local>
2018-05-04 11:14:54 -0400

    Invite tom

--- server.js
+++ server.js
@@ -5,37 +5,30 @@ shgame = require('./sh-game');
 var https = require('https');
 var querystring = require('querystring')
 var Slack = require('slack-node');
-slack = new Slack('xoxb-358077013474-1q4oD2JVr4WMbid3huglPILX');
-
-// slack.api('apps.permission.request', {
-//   user:'D3H23DLBT',
-//   channel:'GAHQZUC6Q'
-// }, function(err, response){
-//   console.log(response);
-// });		
+slack = new Slack('xoxb-358077013474-1q4oD2JVr4WMbid3huglPILX');	
 
 
 app.post('/', function (req, res) {
   // res.send('hello world')
 	console.log(req.body)
 
-	// var message = req.body.event.text
+	var message = req.body.event.text
 
-	// message = message.replace(/.*>/, "")
-	// message = message.replace(/\s/, "")
+	message = message.replace(/.*>/, "")
+	message = message.replace(/\s/, "")
 
-	// console.log(message)
+	console.log(message)
 
-	// if (message == "invite") {
+	if (message == "invite") {
 		 
-	// 	slack.api('channels.invite', {
-	// 	  user:'D3H23DLBT',
-	// 	  channel:'GAHQZUC6Q'
-	// 	}, function(err, response){
-	// 	  console.log(response);
-	// 	});		
+		slack.api('channels.invite', {
+		  user:'D3H23DLBT',
+		  channel:'GAHQZUC6Q'
+		}, function(err, response){
+		  console.log(response);
+		});		
 
-	// }
+	}
 	res.sendStatus(200);
   // res.send(req.body.challenge)
 })
