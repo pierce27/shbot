da5d44223317f3f8c87fa4ad209a1af97c44940b
Andrew Pierce <andrewpierce@Andrews-MacBook-Air.local>
2018-05-04 15:17:25 -0400

    dont remove whitespace

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.post('/', function (req, res) {
 	var user = req.body.event.user;
 
 	message = message.replace(/.*>\s/, "")
-	message = message.replace(/\s/, "")
+	// message = message.replace(/\s/, "")
 
 	console.log(message)
 
