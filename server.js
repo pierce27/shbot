00a271354077a88e0787dfe517bf790e2002d693
Andrew Pierce <andrewpierce@Andrews-MacBook-Air.local>
2018-05-06 21:44:34 -0400

    Add policy logic

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 bodyParser = require('body-parser');
 app = require('express')();
 app.use(bodyParser.json());
-shgame = require('./sh-game');
 var https = require('https');
 var querystring = require('querystring')
 var Slack = require('slack-node');
@@ -24,10 +23,17 @@ var members = [];
 var presidentIndex = 0;
 var president = '';
 var chancellor = '';
+var chancellors = [];
+var failedResolutions = 0;
 
 var yesVotes = [];
 var noVotes = [];
 var votesLeft = 0;
+
+var policies = ['fascist', 'fascist', 'fascist', 'fascist', 'fascist', 'fascist', 'fascist', 'fascist', 'fascist', 'fascist', 'fascist', 'liberal', 'liberal', 'liberal', 'liberal', 'liberal', 'liberal'];
+var presidentialPolicyOptions = [];
+var chancellorPolicyOptions = [];
+var enactedPolicies = [];
 
 
 app.post('/', function (req, res) {
@@ -48,310 +54,361 @@ app.post('/', function (req, res) {
 
 	console.log(message)
 
-	if (message == "new game") {
-
-		if (gameInProgress == true){
-
-		  slack.api('chat.postMessage', {
-		  	channel: channel,
-		  	text: 'Game In Progress'
-
-		  }, function(err, response){
-
-		  	console.log(response)
-
-		  })				
-		} else {
-			gameInProgress == true;
-		}
-
-		
-		// Get Member list
-		slack.api('groups.info', {
-		  // user:'D3H23DLBT',
-		  channel:'GAKDANLBG'
-		}, function(err, response){
-		  console.log(response);
-
-		  members = response.group.members
-
-		  // Remove bot from members
-		  var botIndex = members.indexOf('UAJ290DDY');
-		  if (botIndex >= 0) {
-     		  members.splice( botIndex, 1 );
-	      }		  
-
-		  liberals = members
-
-		  // Set Fascists
-		  while (fascists.length <= numberFascistOptions[members.length - 1]){
-		  	fascistIndex = Math.floor(Math.random()*members.length);
-
-		  	fascists.push(liberals[fascistIndex]);
-
-		  	liberals = liberals.filter(function( obj ) {
-    			return obj.field !== fascists[fascists.length -1];
-		  	})
-		  }
+	if (channel == secretHitlerChannel){
 
 
-		  // Set Hitler
-		  hitler = fascists[Math.floor(Math.random()*fascists.length)];
+		if (message == "new game") {
 
-		  // Remove from fascists
-		  fascists = fascists.filter(function( obj ) {
-			return obj.field !== hitler;
-		  })		  
+			if (gameInProgress == true){
 
-		  // Invite Fascists
-		  for (var i = fascists.length - 1; i >= 0; i--) {
+			  slack.api('chat.postMessage', {
+			  	channel: channel,
+			  	text: 'Game In Progress'
 
-			  slack.api('groups.invite', {
-			  	user: fascists[i],
-			  	channel: facsistChannel
 			  }, function(err, response){
 
 			  	console.log(response)
 
-			  })		  	
-		  }
-
-		  // Invite Hitler
-		  slack.api('groups.invite', {
-		  	user: hitler,
-		  	channel: hitlerChannel
-		  }, function(err, response){
-
-		  	console.log(response)
-
-		  })
-
-		  president = members[presidentIndex];
-
-     	  slack.api('groups.invite', {
-			user: president,
-			channel: presidentChannel
-		  }, function(err, response){
-
-			console.log(response)
-
-		  })			  
-
-		  slack.api('chat.postMessage', {
-		  	channel: secretHitlerChannel,
-		  	text: 'President is now <@' + president + '>. Nominate a Chancelor by @Secret Hitler Bot "I nominate @user" !'
-
-		  }, function(err, response){
-
-		  	console.log(response)
-
-		  })
-
-		  console.log('MEMBERS: ' + members);
-		  console.log('LIBERALS: ' + liberals);
-		  console.log('FASCISTS: ' + fascists);
-		  
-
-		});		
-
-	}
-
-
-
-	// Invite user to secret hitler
-	if (message == 'invite me'){
-
-		if(gameInProgress == true){
-
-		  slack.api('chat.postMessage', {
-		  	channel: channel,
-		  	text: 'Game In Progress'
-
-		  }, function(err, response){
-
-		  	console.log(response)
-
-		  })			
-
-		} else {
-
-		  slack.api('groups.invite', {
-		  	user: user,
-		  	channel: secretHitlerChannel
-		  }, function(err, response){
-
-		  	console.log(response)
-
-		  })				
-
-		}
-
-	}
-
-
-	// End the game
-	if (message == 'end game'){
-
-		gameInProgress = false;
-		fascists = [];
-		hitler = '';
-		liberals = [];
-		members = [];
-		presidentIndex = 0;
-		president = '';
-		chancellor = '';
-		yesVotes = [];
-		noVotes = [];
-		votesLeft = 0;
-
-
-	}
-
-
-	// Nominate chancellor
-	if (message.indexOf('i nominate') !== -1){
-
-		if (user !== president){
-
-			slack.api('chat.postMessage', {
-				channel: secretHitlerChannel,
-				text: 'You cannot nominate chancellor, you are not President!'
-			}, function(err, response){
-
-				console.log(response)
-
-			})
-
-
-		} else {
-			chancellor = message.replace(/.*<@/, '')
-			chancellor = chancellor.replace(/>/, '')
-			chancellor = chancellor.replace(/\s/, '')
-			chancellor = chancellor.toUpperCase()
-
-			slack.api('chat.postMessage', {
-				channel: secretHitlerChannel,
-				text: 'Vote if you would like <@' + chancellor +'> as chancellor by telling me I vote yes or I vote no'
-			}, function(err, response){
-
-				console.log(response)
-
-			})						
-
-		}
-
-	}
-
-
-	// Vote for chancellor
-	if (message.indexOf('i vote') !== -1 && req.body.event.type == 'message'){
-
-		if(yesVotes.indexOf(user) == -1 && noVotes.indexOf(user) == -1){
-			var vote = message.replace(/i vote /,'')
-
-			if(vote == 'yes'){
-				yesVotes.push(user);
-			} else if (vote == 'no'){
-				noVotes.push(user);
+			  })				
 			} else {
-				slack.api('chat.postMessage', {
-					channel: secretHitlerChannel,
-					text: 'Vote for <@' + chancellor +'> as chancellor by telling me "I vote yes" or "I vote no"'
-				}, function(err, response){
-
-					console.log(response)
-
-				})					
+				gameInProgress == true;
 			}
 
-			console.log('YES VOTES: ' + yesVotes);
-			console.log('NO VOTES: ' + noVotes);
+			
+			// Get Member list
+			slack.api('groups.info', {
+			  // user:'D3H23DLBT',
+			  channel:'GAKDANLBG'
+			}, function(err, response){
+			  console.log(response);
 
-			var totalVotes = yesVotes.length + noVotes.length;
+			  members = response.group.members
 
-			if(totalVotes < members.length){
+			  // Remove bot from members
+			  var botIndex = members.indexOf('UAJ290DDY');
+			  if (botIndex >= 0) {
+	     		  members.splice( botIndex, 1 );
+		      }		  
+
+			  liberals = members
+
+			  // Set Fascists
+			  while (fascists.length <= numberFascistOptions[members.length - 1]){
+			  	fascistIndex = Math.floor(Math.random()*members.length);
+
+			  	fascists.push(liberals[fascistIndex]);
+
+			  	liberals = liberals.filter(function( obj ) {
+	    			return obj.field !== fascists[fascists.length -1];
+			  	})
+			  }
 
 
-				
+			  // Set Hitler
+			  hitler = fascists[Math.floor(Math.random()*fascists.length)];
+
+			  // Remove from fascists
+			  fascists = fascists.filter(function( obj ) {
+				return obj.field !== hitler;
+			  })		  
+
+			  // Invite Fascists
+			  for (var i = fascists.length - 1; i >= 0; i--) {
+
+				  slack.api('groups.invite', {
+				  	user: fascists[i],
+				  	channel: facsistChannel
+				  }, function(err, response){
+
+				  	console.log(response)
+
+				  })		  	
+			  }
+
+			  // Invite Hitler
+			  slack.api('groups.invite', {
+			  	user: hitler,
+			  	channel: hitlerChannel
+			  }, function(err, response){
+
+			  	console.log(response)
+
+			  })
+
+			  president = members[presidentIndex];
+
+	     	  slack.api('groups.invite', {
+				user: president,
+				channel: presidentChannel
+			  }, function(err, response){
+
+				console.log(response)
+
+			  })			  
+
+			  slack.api('chat.postMessage', {
+			  	channel: secretHitlerChannel,
+			  	text: 'President is now <@' + president + '>. Nominate a Chancelor by @Secret Hitler Bot "I nominate @user" !'
+
+			  }, function(err, response){
+
+			  	console.log(response)
+
+			  })
+
+			  console.log('MEMBERS: ' + members);
+			  console.log('LIBERALS: ' + liberals);
+			  console.log('FASCISTS: ' + fascists);
+			  
+
+			});		
+
+		}
+
+
+
+		// Invite user to secret hitler
+		if (message == 'invite me'){
+
+			if(gameInProgress == true){
+
+			  slack.api('chat.postMessage', {
+			  	channel: channel,
+			  	text: 'Game In Progress'
+
+			  }, function(err, response){
+
+			  	console.log(response)
+
+			  })			
+
+			} else {
+
+			  slack.api('groups.invite', {
+			  	user: user,
+			  	channel: secretHitlerChannel
+			  }, function(err, response){
+
+			  	console.log(response)
+
+			  })				
+
+			}
+
+		}
+
+
+		// End the game
+		if (message == 'end game'){
+
+			gameInProgress = false;
+			fascists = [];
+			hitler = '';
+			liberals = [];
+			members = [];
+			presidentIndex = 0;
+			president = '';
+			chancellor = '';
+			yesVotes = [];
+			noVotes = [];
+			votesLeft = 0;
+
+
+		}
+
+
+		// Nominate chancellor
+		if (message.indexOf('i nominate') !== -1){
+
+			if (user !== president){
+
 				slack.api('chat.postMessage', {
 					channel: secretHitlerChannel,
-					text:  votesLeft + ' votes remaining, ' + yesVotes.length + ' voted yes, ' + noVotes.length + ' voted no'
+					text: 'You cannot nominate chancellor, you are not President!'
 				}, function(err, response){
 
 					console.log(response)
 
-				})					
+				})
+
 
 			} else {
+				chancellor = message.replace(/.*<@/, '')
+				chancellor = chancellor.replace(/>/, '')
+				chancellor = chancellor.replace(/\s/, '')
+				chancellor = chancellor.toUpperCase()
 
-				if(noVotes.length > yesVotes.length){
-					if(presidentIndex < members.length - 1){
-						presidentIndex = presidentIndex + 1;
-					} else {
-						presidentIndex = 0;
-					}
+				slack.api('chat.postMessage', {
+					channel: secretHitlerChannel,
+					text: 'Vote if you would like <@' + chancellor +'> as chancellor by telling me I vote yes or I vote no'
+				}, function(err, response){
 
-					slack.api('groups.kick', {
-						user: president,
-						channel: presidentChannel
-					}, function(err, response){
+					console.log(response)
 
-						console.log(response)
+				})						
 
-					})						
+			}
 
-					president = members[presidentIndex];
+		}
 
+
+		// Vote for chancellor
+		if (message.indexOf('i vote') !== -1 && req.body.event.type == 'message'){
+
+			if(yesVotes.indexOf(user) == -1 && noVotes.indexOf(user) == -1){
+				var vote = message.replace(/i vote /,'')
+
+				if(vote == 'yes'){
+					yesVotes.push(user);
+				} else if (vote == 'no'){
+					noVotes.push(user);
+				} else {
 					slack.api('chat.postMessage', {
 						channel: secretHitlerChannel,
-						text: 'Resolution for <@' + chancellor + '> as chancellor not passed <@' + president + '> is now president. Nominate a chancellor!'
+						text: 'Vote for <@' + chancellor +'> as chancellor by telling me "I vote yes" or "I vote no"'
 					}, function(err, response){
 
-						console.log(response)
+						// console.log(response)
 
-					})
+					})					
+				}
 
-					slack.api('groups.invite', {
-						user: president,
-						channel: presidentChannel
+				console.log('YES VOTES: ' + yesVotes);
+				console.log('NO VOTES: ' + noVotes);
+
+				var totalVotes = yesVotes.length + noVotes.length;
+
+				if(totalVotes < members.length){
+
+
+					
+					slack.api('chat.postMessage', {
+						channel: secretHitlerChannel,
+						text:  votesLeft + ' votes remaining, ' + yesVotes.length + ' voted yes, ' + noVotes.length + ' voted no'
 					}, function(err, response){
 
-						console.log(response)
+						// console.log(response)
 
-					})	
-
-					yesVotes = []
-					noVotes = []										
+					})					
 
 				} else {
 
-					slack.api('chat.postMessage', {
-						channel: secretHitlerChannel,
-						text: 'The resolution for <@' + chancellor + '> as chancellor has passed!!'
-					}, function(err, response){
+					if(noVotes.length > yesVotes.length){
+						if(presidentIndex < members.length - 1){
+							presidentIndex = presidentIndex + 1;
+						} else {
+							presidentIndex = 0;
+						}
 
-						console.log(response)
+						slack.api('groups.kick', {
+							user: president,
+							channel: presidentChannel
+						}, function(err, response){
 
-					})
+							console.log(response)
 
-					// TODO Resolution logic
+						})						
+
+						president = members[presidentIndex];
+
+						slack.api('chat.postMessage', {
+							channel: secretHitlerChannel,
+							text: 'Resolution for <@' + chancellor + '> as chancellor not passed <@' + president + '> is now president. Nominate a chancellor!'
+						}, function(err, response){
+
+							// console.log(response)
+							failedResolutions = failedResolutions + 1;
+
+						})
+
+						slack.api('groups.invite', {
+							user: president,
+							channel: presidentChannel
+						}, function(err, response){
+
+							console.log(response)
+
+						})	
+
+						yesVotes = []
+						noVotes = []										
+
+					} else {
+
+						slack.api('chat.postMessage', {
+							channel: secretHitlerChannel,
+							text: 'The resolution for <@' + chancellor + '> as chancellor has passed!!'
+						}, function(err, response){
+
+							// console.log(response)
+							chancellors.push(chancellor);
+							failedResolutions = 0;
+
+						})
+
+						// TODO Resolution logic
+					    slack.api('groups.invite', {
+					  	  user: chancellor,
+					  	  channel: chancellorChannel
+					    }, function(err, response){
+
+					  	  console.log(response)
+
+					    })	
+
+					    // Determine options for president
+					   var i = 0;
+					   while(i < 3 ){
+
+					   	policyIndex = Math.floor(Math.random()*policies.length);
+
+					   	presidentialPolicyOptions.push(policies[policyIndex]);
+
+					   	policies.splice(policyIndex, 1);
+
+					   	i = i + 1;
+
+					   	// Once options are set than notify president. 
+					   	if (i == 3) {
+
+							slack.api('chat.postMessage', {
+								channel: presidentChannel,
+								text: 'Choose between these policies ' + presidentialPolicyOptions + ' by typing policies you want chancellor to choose from separated by a space (ex. "fascist, liberal"'
+							}, function(err, response){
+
+
+							})		
+									   		
+					   	}
+
+					   }	
+
+
+					}
 				}
+
+
+			} else {
+
+				slack.api('chat.postMessage', {
+					channel: secretHitlerChannel,
+					text: 'You already voted <@' + user + '>'
+				}, function(err, response){
+
+					// console.log(response)
+
+				})			
+
 			}
-
-
-		} else {
-
-			slack.api('chat.postMessage', {
-				channel: secretHitlerChannel,
-				text: 'You already voted <@' + user + '>'
-			}, function(err, response){
-
-				console.log(response)
-
-			})			
-
 		}
 	}
+
+	if (channel == presidentChannel) {
+		
+	}
+
+
+
 
 
 	res.sendStatus(200);
