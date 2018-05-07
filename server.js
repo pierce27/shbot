81958296216bb59ed985677263efbb1d91e98232
Andrew Pierce <andrewpierce@Andrews-MacBook-Air.local>
2018-05-06 23:01:13 -0400

    Put logic in button

--- server.js
+++ server.js
@@ -467,15 +467,145 @@ app.post('/', function (req, res) {
 
 app.post('/component', function(req,res){
 	
-	var body = qs.parse(req.body)
+	var payload = qs.parse(req.body.payload)
+	var vote = payload.actions[0].value
+	var user = payload.user.id
 
-	// if (payload.callback_id == "chancellor_vote"){
-	// 	if(payload.actions.value == "yes"){
-	// 		yesVotes.push.
-	// 	}
-	// }
+	if (payload.callback_id == "chancellor_vote"){
 
-	console.log(body.payload)
+		if(vote == 'yes'){
+			yesVotes.push(user);
+		} else if (vote == 'no'){
+			noVotes.push(user);
+		} else {
+			slack.api('chat.postMessage', {
+				channel: secretHitlerChannel,
+				text: 'Vote for <@' + chancellor +'> as chancellor by telling me "I vote yes" or "I vote no"'
+			}, function(err, response){
+
+				// console.log(response)
+
+			})					
+		}
+
+		console.log('YES VOTES: ' + yesVotes);
+		console.log('NO VOTES: ' + noVotes);
+
+		var totalVotes = yesVotes.length + noVotes.length;
+
+		if(totalVotes < members.length){
+
+
+			
+			slack.api('chat.postMessage', {
+				channel: secretHitlerChannel,
+				text:  votesLeft + ' votes remaining, ' + yesVotes.length + ' voted yes, ' + noVotes.length + ' voted no'
+			}, function(err, response){
+
+				// console.log(response)
+
+			})					
+
+		} else {
+
+			if(noVotes.length > yesVotes.length){
+				if(presidentIndex < members.length - 1){
+					presidentIndex = presidentIndex + 1;
+				} else {
+					presidentIndex = 0;
+				}
+
+				slack.api('groups.kick', {
+					user: president,
+					channel: presidentChannel
+				}, function(err, response){
+
+					console.log(response)
+
+				})						
+
+				president = members[presidentIndex];
+
+				slack.api('chat.postMessage', {
+					channel: secretHitlerChannel,
+					text: 'Resolution for <@' + chancellor + '> as chancellor not passed <@' + president + '> is now president. Nominate a chancellor!'
+				}, function(err, response){
+
+					// console.log(response)
+					failedResolutions = failedResolutions + 1;
+
+				})
+
+				slack.api('groups.invite', {
+					user: president,
+					channel: presidentChannel
+				}, function(err, response){
+
+					console.log(response)
+
+				})	
+
+				yesVotes = []
+				noVotes = []										
+
+			} else {
+
+				slack.api('chat.postMessage', {
+					channel: secretHitlerChannel,
+					text: 'The resolution for <@' + chancellor + '> as chancellor has passed!!'
+				}, function(err, response){
+
+					// console.log(response)
+					chancellors.push(chancellor);
+					failedResolutions = 0;
+
+				})
+
+				// TODO Resolution logic
+			    slack.api('groups.invite', {
+			  	  user: chancellor,
+			  	  channel: chancellorChannel
+			    }, function(err, response){
+
+			  	  console.log(response)
+
+			    })	
+
+			    // Determine options for president
+			   var i = 0;
+			   while(i < 3 ){
+
+			   	policyIndex = Math.floor(Math.random()*policies.length);
+
+			   	presidentialPolicyOptions.push(policies[policyIndex]);
+
+			   	policies.splice(policyIndex, 1);
+
+			   	i = i + 1;
+
+			   	// Once options are set than notify president. 
+			   	if (i == 3) {
+
+					slack.api('chat.postMessage', {
+						channel: presidentChannel,
+						text: 'Choose between these policies ' + presidentialPolicyOptions + ' by typing policies you want chancellor to choose from separated by a space (ex. "fascist, liberal"'
+					}, function(err, response){
+
+
+					})		
+							   		
+			   	}
+
+			   }	
+
+
+			}
+		}
+	}
+
+	
+
+	console.log(body.payload.actions[0])
 	res.sendStatus(200)
 })
 
