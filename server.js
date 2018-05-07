8a0714be64dfb466f49687d488e700c821e2acb5
Andrew Pierce <andrewpierce@Andrews-MacBook-Air.local>
2018-05-07 11:13:54 -0400

    Test button

--- server.js
+++ server.js
@@ -468,7 +468,7 @@ app.post('/', function (req, res) {
 app.post('/component', function(req,res){
 	
 
-	console.log(req.body.payload)
+	console.log(req.body.payload['actions'])
 	
 
 	// if (payload.callback_id == "chancellor_vote"){
